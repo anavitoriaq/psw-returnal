f6c7bed2d0d1b907515b6e0e896fece6e396f6de
anavitoriaq <anavitoriaqueiroz78@gmail.com>
2025-01-13 21:50:53 -0300

    feat: Versão inicial dos projetos implementadas e funcionando

---
+++ pythonando-psw-returnal/templates/app.py
@@ -0,0 +1,58 @@
+import __init__
+from views.view import SubscriptionSevice
+from models.database import engine
+from models.model import Subscription
+from datetime import datetime
+from decimal import Decimal
+
+class UI:
+    def __init__(self):
+        self.subscription_service = SubscriptionSevice(engine)
+
+
+    def start(self):
+        while True:
+            print('''
+            [1] -> Adicionar assinatura
+            [2] -> Remover assinatura
+            [3] -> Valor total
+            [4] -> Gastos últimos 12 meses
+            [5] -> Sair
+            ''')
+            choice = int(input('Escolha uma opção: '))
+            if choice == 1:
+                self.add_subscription()
+            elif choice == 2:
+                self.delete_subscription()
+            elif choice == 3:
+                self.total_value()
+            elif choice == 4:
+                self.subscription_service.gen_chart()
+                #TODO: chamar o método pay na interface
+            else:
+                break
+
+    def add_subscription(self):
+        empresa = input('Empresa: ')
+        site = input('Site: ')
+        data_assinatura = datetime.strftime(input('Data da assinatura: '),'%d/%m/%Y')
+        valor = Decimal(input('Valor: '))
+        subscription = Subscription(empresa=empresa, site=site, data_assinatura=data_assinatura, valor=valor)
+        self.subscription_service.creat(subscription)
+
+    def delete_subscription(self):
+        subscription = self.subscription_service.list_all()
+        #TODO: quando excluir a assinatura excluir todos os pagamentos dela
+        print('Escolha qual assinatura deseja excluir')
+        
+        for i in subscription:
+            print(f'[{i.id}] -> {i.empresa}')
+
+        choice = int(input('Escolha a assinatura: '))
+        self.subscription_service.delete(choice)
+        print('Assinatura excluida com sucesso!')
+
+    def total_value(self):
+        print('Seu valor total mensal em assinatura é: {self.subscription_service.total_value()}')
+
+UI().start()
